02b7978f9d9fc9e45d41bb774bb113bd5561cfa5
ali.sh <narenje20142014@gmail.com>
2021-08-24 21:01:28 +0430

    start prj-hackNews

---
+++ react-hacker-news/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchForm from './components/SearchForm'
+import Stories from './components/Stories'
+import Buttons from './components/Buttons'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hacker news</h1>
+    </div>
+  )
+}
+
+export default App
