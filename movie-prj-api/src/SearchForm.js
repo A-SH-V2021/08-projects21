ada034ffd0815f7da55af3999d37df6f6ac37e7d
ali.sh <narenje20142014@gmail.com>
2021-08-28 03:06:36 +0430

    route  app

--- movie-prj-api/src/SearchForm.js
+++ movie-prj-api/src/SearchForm.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 const SearchForm = () => {
-  return <h2>search component</h2>
-}
+  return (
+    <form className="search-form">
+      <h2>serach</h2>
+      <input type="text" className="form-input" />
+      <p className='error'>error</p>
+    </form>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
