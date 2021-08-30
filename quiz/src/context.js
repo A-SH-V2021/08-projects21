b2762195b52d2aa8d33e4cecac76717773fc219e
ali.sh <narenje20142014@gmail.com>
2021-08-30 04:32:02 +0430

    define states

--- quiz/src/context.js
+++ quiz/src/context.js
@@ -1,24 +1,37 @@
-import axios from 'axios'
-import React, { useState, useContext, useEffect } from 'react'
+import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
 
 const table = {
   sports: 21,
   history: 23,
   politics: 24,
-}
+};
 
-const API_ENDPOINT = 'https://opentdb.com/api.php?'
+const API_ENDPOINT = "https://opentdb.com/api.php?";
 
-const url = ''
+const url = "";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [correct, setCorrect] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [error, setError] = useState(false);
+  const [modal, setModal] = useState(false);
+  return (
+    <AppContext.Provider
+      value={{ waiting, loading, questions, correct, index, error, modal }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
