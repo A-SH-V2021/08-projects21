c773479abc4ee9b8a68da7b37ac9201133c6acf1
ali.sh <narenje20142014@gmail.com>
2021-08-30 23:35:24 +0430

    fetching data

--- quiz/src/context.js
+++ quiz/src/context.js
@@ -9,7 +9,7 @@ const table = {
 
 const API_ENDPOINT = "https://opentdb.com/api.php?";
 
-const url = "";
+const url = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy";
 
 const AppContext = React.createContext();
 
@@ -21,6 +21,33 @@ const AppProvider = ({ children }) => {
   const [index, setIndex] = useState(0);
   const [error, setError] = useState(false);
   const [modal, setModal] = useState(false);
+
+  const fetching = async () => {
+    
+    setLoading(true);
+    setWaiting(false)
+    const response = await axios(url).catch((err) =>
+      console.log(`error in get axios:`, err)
+    );
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setWaiting(false);
+        setLoading(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetching();
+  }, []);
   return (
     <AppContext.Provider
       value={{ waiting, loading, questions, correct, index, error, modal }}
